64adefb0bfa07fc795841ce3fa8d819f9f989724
aecio prado <aecioprado@gmail.com>
2021-12-08 20:03:37 -0300

    Lancamento Dto

--- src/main/java/com/aecioprado/mv/dto/LancamentoDto.java
+++ src/main/java/com/aecioprado/mv/dto/LancamentoDto.java
@@ -1,93 +1,24 @@
 package com.aecioprado.mv.dto;
 
-import java.io.Serializable;
-import java.util.Objects;
+import javax.validation.constraints.NotBlank;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
-public class LancamentoDto implements Serializable {
-	
-
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 5224811368963241639L;
-	public LancamentoDto(Long id, String colaborador, String cpf, String produtos) {
-		this.id = id;
-		this.colaborador = colaborador;
-		this.cpf = cpf;
-		this.produtos = produtos;
-	}
-	
-	public LancamentoDto() {
-	}
+@Data
+@NoArgsConstructor
+public class LancamentoDto{
 	
 	private Long id;
+	
+	@NotBlank(message = "Informe o nome do colaborador.")
 	private String colaborador;
+	
+	@NotBlank(message = "Informe o cpf do colaborador.")
 	private String cpf;
+	
+	@NotBlank(message = "Informe os itens do cafe da manha.")
 	private String produtos;
 	
-	
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getColaborador() {
-		return colaborador;
-	}
-
-	public void setColaborador(String colaborador) {
-		this.colaborador = colaborador;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public String getProdutos() {
-		return produtos;
-	}
-
-	public void setProdutos(String produtos) {
-		this.produtos = produtos;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(colaborador, cpf, id, produtos);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		LancamentoDto other = (LancamentoDto) obj;
-		return Objects.equals(colaborador, other.colaborador) && Objects.equals(cpf, other.cpf)
-				&& Objects.equals(id, other.id) && Objects.equals(produtos, other.produtos);
-	}
-
-	@Override
-	public String toString() {
-		return "LancamentoDto [id=" + id + ", colaborador=" + colaborador + ", cpf=" + cpf + ", produtos=" + produtos
-				+ "]";
-	}
-	
-	
-	
-
 }
