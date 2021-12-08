ba5b3482896c5ce9388bf4ebce0aacb6d123875e
aecio prado <aecioprado@gmail.com>
2021-12-08 20:00:41 -0300

    Configuracao cache

--- src/main/java/com/aecioprado/mv/service/ILancamentoService.java
+++ src/main/java/com/aecioprado/mv/service/ILancamentoService.java
@@ -3,27 +3,30 @@ package com.aecioprado.mv.service;
 import java.util.List;
 import java.util.Optional;
 
+import com.aecioprado.mv.dto.LancamentoDto;
 import com.aecioprado.mv.entity.LancamentoEntity;
 
 public interface ILancamentoService {
 	
-	// define quais métodos deverão ser implementados
+	//CRUD
 	
 	//salvar
-	public LancamentoEntity salvar(LancamentoEntity lancamento);
+	public Boolean salvar(LancamentoDto lancamento);
 	
 	//atualizar
-	public LancamentoEntity atualizar(LancamentoEntity lancamento);
+	public Boolean atualizar(LancamentoDto lancamento);
 	
 	//consultar por Id
-	Optional<LancamentoEntity> obterPorId(Long id);
+	public LancamentoEntity obterPorId(Long id);
 	
 	//excluir
 	public boolean excluir(Long id); 
 	
-	
 	// listar todos os lancamentos
 	public List<LancamentoEntity> listarTodos();
+	
+	
+	//métodos de validação:
 	
 	//validar se cpf existe
 	public void validarCpf(String cpf);
--- src/main/java/com/aecioprado/mv/service/LancamentoServiceImp.java
+++ src/main/java/com/aecioprado/mv/service/LancamentoServiceImp.java
@@ -1,73 +1,114 @@
 package com.aecioprado.mv.service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.NoSuchElementException;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import com.aecioprado.mv.dto.LancamentoDto;
 import com.aecioprado.mv.entity.LancamentoEntity;
 import com.aecioprado.mv.exception.LancamentoException;
 import com.aecioprado.mv.repository.ILancamentoRepository;
 
+@CacheConfig(cacheNames = "lancamento")
 @Service
 public class LancamentoServiceImp implements ILancamentoService {
+	
+	private static final String MSG_ELEMENTO_NAO_EXISTE = "O item nao existe no sistema: ";
+	private static final String MSG_CPF_JA_CADASTRADO = "O CPF informado já está presente na lista do café da manha: ";
+	private static final String MENSAGEM_ERRO_INTERNO = "Erro interno identificado. Contate o suporte";
+	
 
+	private ILancamentoRepository lancamentoRepository;
+	private ModelMapper mapper;
+	
 	@Autowired
-	private ILancamentoRepository lancamentoRepo;
-
-	// salvar lancamento
-	@Override
-	@Transactional
-	public LancamentoEntity salvar(LancamentoEntity lancamento) {
-		validarCpf(lancamento.getCpf());
-		validarProdutoExiste(lancamento.getProdutos());
-		return this.lancamentoRepo.save(lancamento);
+	public LancamentoServiceImp(ILancamentoRepository lancamentoRepository) {
+		this.mapper = new ModelMapper();
+		this.lancamentoRepository = lancamentoRepository;
+		
 	}
 
-	// atualizar lancamento
+	// salvar
 	@Override
-	@Transactional
-	public LancamentoEntity atualizar(LancamentoEntity lancamento) {
-		Objects.requireNonNull(lancamento.getId());
-		validarProdutoExiste(lancamento.getProdutos());
-		return this.lancamentoRepo.save(lancamento);
+	public Boolean salvar(LancamentoDto lancamentoDto) {
+
+		try {
+			validarCpf(lancamentoDto.getCpf());
+			validarProdutoExiste(lancamentoDto.getProdutos());
+			
+			LancamentoEntity lancamentoEntity = this.mapper.map(lancamentoDto, LancamentoEntity.class);
+			this.lancamentoRepository.save(lancamentoEntity);
+			return true;
+		} catch (Exception e) {
+			throw new LancamentoException(e.getMessage(),HttpStatus.OK);
+	}
 	}
 
-	// excluir lancamento
+	// atualizar
+	@CacheEvict(key="#lancamentoDto.id")
 	@Override
-	@Transactional
+	public Boolean atualizar(LancamentoDto lancamentoDto) {
+
+		try {
+				this.lancamentoRepository.findById(lancamentoDto.getId()).get();
+
+				LancamentoEntity lancamentoEntityAtualizado = this.mapper.map(lancamentoDto, LancamentoEntity.class);
+				this.lancamentoRepository.save(lancamentoEntityAtualizado);
+				return true;
+		}catch (LancamentoException m) {
+			throw m;
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+
+
+	// excluir
+	@Override
 	public boolean excluir(Long id) {
 		try {
-			this.lancamentoRepo.deleteById(id);
+			this.lancamentoRepository.deleteById(id);
 			return true;
 
-		} catch (LancamentoException e) {
-			return false;
+		} catch (Exception e) {
+			throw new LancamentoException(MSG_ELEMENTO_NAO_EXISTE + id, HttpStatus.NOT_FOUND);
 		}
 	}
 
+	// listar Todos
 	@Override
 	public List<LancamentoEntity> listarTodos() {
-		return this.lancamentoRepo.findAll();
+		return this.lancamentoRepository.findAll();
+}
+
+	// listar por id
+	@Cacheable(key="#id")
+	@Override
+	public LancamentoEntity obterPorId(Long id) { //refatorar
+		try {
+		return this.lancamentoRepository.findById(id).get();
+		} catch (NoSuchElementException e) {
+			throw new LancamentoException(MSG_ELEMENTO_NAO_EXISTE + id, HttpStatus.NOT_FOUND);
+		}
 
 	}
-
+	
 	@Override
 	public void validarCpf(String cpf) {
-		boolean existe = lancamentoRepo.existsByCpf(cpf);
+		
+		boolean existe = this.lancamentoRepository.existsByCpf(cpf);
 		if (existe) {
-			throw new LancamentoException("Atenção: O CPF informado já está presente na lista do café da manha");
+			throw new LancamentoException(MSG_CPF_JA_CADASTRADO + cpf, HttpStatus.OK);
 		}
-	}
-
-	@Override
-	public Optional<LancamentoEntity> obterPorId(Long id) {
-		return this.lancamentoRepo.findById(id);
-
 	}
 
 	@Override
@@ -75,11 +116,20 @@ public class LancamentoServiceImp implements ILancamentoService {
 		
 		//transforma a String em tokens
 		String stringTemp = produtos;
-		String[] produtosTokens = stringTemp.split(",");
+		
+		//adicionar regex no split para remover os espacos em branco
+		//String[] produtosTokens = stringTemp.split(",");
+		String[] produtosTokens = stringTemp.split(",\\s+");
+		
+		List array_temp = Arrays.asList(produtosTokens);
+		
+		System.out.println(array_temp);
 		
 		//recupera uma lista com todos os registros do banco
 		List<LancamentoEntity> todosLancamentos = new ArrayList<LancamentoEntity>();
-		todosLancamentos = lancamentoRepo.findAll();
+		
+		//criar um uma nova consulta para retornar os produtos direto do banco.
+		todosLancamentos = lancamentoRepository.findAll();
 		
 		//adiciona numa nova lista apenas os produtos
 		List<String> listaProdutos = new ArrayList<String>();
@@ -95,7 +145,7 @@ public class LancamentoServiceImp implements ILancamentoService {
 			for(int j=0; j<listaProdutos.size(); j++) {
 				if (listaProdutos.get(j) != null && listaProdutos.get(j).contains(produtosTokens[i])) {
 					//Lança exception assim que houve o primeiro "match"
-					throw new LancamentoException("Um ou mais produtos da sua lista já foi cadastrado");
+					throw new LancamentoException("Um ou mais produtos da sua lista já foi cadastrado", HttpStatus.OK);
 				}
 			}
 		}
